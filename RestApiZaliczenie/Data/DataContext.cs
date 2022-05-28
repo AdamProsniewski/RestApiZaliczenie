f1128af782d66a2774922c7adbb4a4bc4e6aa6c5
Jakub Prach <jakub.prach76@gmail.com>
2022-05-28 14:33:10 +0200

    setting up DbContext

---
+++ RestApiZaliczenie/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestApiZaliczenie.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){
+
+        }
+    }
+}
