ea978efffa59bba29259779daea55feb872e1cd4
Jakub Prach <jakub.prach76@gmail.com>
2022-05-29 10:33:42 +0200

    Empty testing function

---
+++ ActivitiesUnitTests/ActivitiesControllerTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RestApiZaliczenie;
+using RestApiZaliczenie.Controllers;
+using RestApiZaliczenie.Data;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ActivitiesUnitTests
+{
+    public class ActivitiesControllerTests
+    {
+
+        [Fact]
+        public async Task GetActivity()
+        {
+
+        }
+    }
+}
